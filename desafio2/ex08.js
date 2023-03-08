74b07c1ba01fd2af8b9f3159726216f0190a44d2
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-03-08 14:00:04 -0300

    resolvido os exercicios 8 e 9

--- desafio2/ex08.js
+++ desafio2/ex08.js
@@ -2,6 +2,19 @@
 
 
 
-let mynumber = 65;
+let mynumber = 27;
 
-if(mynumber % 2 === 0 && )
+if (mynumber % 2 === 0) {
+
+    if (mynumber % 5 === 0) console.log(`O numero ${mynumber} é par e divisivel por 5`)
+    else console.log(`O numero ${mynumber} é par e NÃO e divisivel por 5`)
+} else {
+
+    for (let i = 2; i < mynumber; i++)
+        if (mynumber % i === 0) {
+          console.log("O numero é impar, mas não é primo")
+          break
+        } else {
+            if(i === mynumber - 1) console.log("O numero é primo")
+    }
+}
--- desafio2/ex09.js
+++ desafio2/ex09.js
@@ -1,3 +1,14 @@
  //[ ] Faça um programa onde ele leia 4 números. Em cada caso, ele deve retornar a mensagem condizente: - Todos os números são impares => TODOS OS NÚMEROS SÃO IMPARES - Todos os números são pares => TODOS OS NÚMEROS SÃO PARES. Em caso negativo dos casos a cima, colocar uma mensagem => Há números pares e ímpares.
 
- 
+ const numberOne = 31;
+ const numbertwo = 35;
+ const numberthree = 23;
+ const numberfour = 5;
+
+ if(numberOne % 2 === 0 && numbertwo % 2 === 0 && numberthree % 2 === 0 && numberfour % 2 === 0) {
+    console.log("Todos o numeros são pares")
+ }else if (numberOne % 2 !== 0 && numbertwo % 2 !== 0 && numberthree % 2 !== 0 && numberfour % 2 !== 0){
+    console.log("Todos os numeros são impares")
+ }else {
+    console.log("tem numeros pares e impares")
+ }
