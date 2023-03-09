6bb0094cee0dcce1945f73d6a082f04728b187a5
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-03-09 02:42:29 -0300

    pratica criando uma tabuada

---
+++ tabuada.js
@@ -0,0 +1,11 @@
+
+const tabuada = tabuadaDo => {
+
+    for(let i = 1; i <= 10; i++){
+        console.log(`${tabuadaDo} X ${i} = ${tabuadaDo * i}`)
+    }
+
+    return
+}
+
+const result = tabuada(6);
