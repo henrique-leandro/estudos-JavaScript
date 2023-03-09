f0e98f8e30ebdfa31cfed3a15323a8f618de8d74
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-03-09 02:42:05 -0300

    exercicio criando uma calculadora

---
+++ calculadora.js
@@ -0,0 +1,37 @@
+/*
+   calculadora usando funçoes
+
+   vai ser a função de somar, subtrair, multiplicar, dividir
+*/
+
+
+const calcular = (number1, number2, operation) => {
+
+    let result
+
+    switch (operation) {
+        case "+":
+            result = number1 + number2
+            break;
+
+        case "-":
+         result = number1 - number2 
+        break;
+
+        case "*":
+            result = number1 * number2 
+            break;
+
+        case "/":
+            result = number1 / number2 
+            break;
+    
+        default:
+            result = number1 + number2
+            break;
+    }
+    
+    return result
+}
+
+console.log(calcular(140, 5, '/'))
