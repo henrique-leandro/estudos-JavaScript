883fcb30e82d111b85ba3b6bab66faa2dd7962c7
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-03-23 21:45:45 -0300

    adicionado arquivo javascript

--- JAVACRIPT WEB/projeto conversor de moeda/assets/script.js
+++ JAVACRIPT WEB/projeto conversor de moeda/assets/script.js
@@ -1,26 +1,64 @@
-const bandeira = document.querySelector(".bandeira")
-const selectCurrent = document.querySelector(".select-current")
-const currentText = document.querySelectorAll('.current-text')[1]
-const currentValue = document.querySelectorAll('.current-value')[1]
-const button = document.querySelector('button')
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+
+const dolar = 5.2
+const euro = 6
 
 
 
 
-const changeCurrent = () => {
+
+
+const convertValues = () => {
+  const inputReais = document.getElementById('input-reais').value
+  const realValueText = document.getElementById('real-value-text')
+  const currencyValueText = document.getElementById('currency-value-text')
+
+
+
+  //realValueText.innerHTML = inputReais
+
+  realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+    { style: 'currency',
+     currency: 'BRL' }
+  ).format(inputReais)
+
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+    { style: 'currency',
+     currency: 'USD' }
+  ).format(inputReais / dolar)
+
+}
+    /*
+    euro
+    currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+    { style: 'currency',
+     currency: 'EUR' }
+  ).format(inputReais / euro)
+    */
+  
+
+  //  currencyValueText.innerHTML = (inputReais / dolar)
+ 
+const changeCurrency = () => {
+  
+  const currencyName = document.getElementById('currency-name')
+  const currencyImg= document.getElementById('currency-img')
+
+  if(select.value == 'US$ Dólar Americano'){
+
+    currencyName.innerHTML = 'Dólar Americano'
+    currencyImg.setAttribute('src', 'assets/bandeira-eua.svg')
+  }
+
+  if(select.value == '€ Euro'){
+    currencyName.innerHTML = '€ Euro'
+    currencyImg.setAttribute('src', 'assets/euro.svg')
+  }
     
-if(selectCurrent.value == "US$ Dólar Americano"){
+    
+  }
 
-   bandeira.setAttribute('src', 'assets/bandeira-eua.svg')
-   currentText.innerHTML = 'Dólar Americano'
-   currentValue.innerHTML = 'US$ 2.000,00'
-}else{
-  bandeira.setAttribute('src', 'assets/euro.svg')
-  currentText.innerHTML = 'Euro'
-  currentValue.innerHTML = '€ 1.800,00'
-}
-
-}
-
-
-selectCurrent.addEventListener('change', changeCurrent)
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
