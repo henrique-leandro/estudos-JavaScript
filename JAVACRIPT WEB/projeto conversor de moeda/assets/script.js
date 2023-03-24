f6a6e1f792d9347460862c02d2e00f07b7d89eb6
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-03-24 16:56:53 -0300

    adicionado bitcoin ao projeto

--- JAVACRIPT WEB/projeto conversor de moeda/assets/script.js
+++ JAVACRIPT WEB/projeto conversor de moeda/assets/script.js
@@ -3,7 +3,8 @@ const select = document.getElementById('currency-select')
 
 
 const dolar = 5.2
-const euro = 6
+const euro = 5.9
+const bitcoin = 148178.04
 
 
 
@@ -17,48 +18,70 @@ const convertValues = () => {
 
 
 
-  //realValueText.innerHTML = inputReais
-
   realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
-    { style: 'currency',
-     currency: 'BRL' }
+    {
+      style: 'currency',
+      currency: 'BRL'
+    }
   ).format(inputReais)
 
+
+  if(select.value === "US$ Dólar Americano") {
     currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
-    { style: 'currency',
-     currency: 'USD' }
-  ).format(inputReais / dolar)
+      {
+        style: 'currency',
+        currency: 'USD'
+      }
+    ).format(inputReais / dolar)
+  }
+
+  if (select.value === "€ Euro") {
+    currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+      {
+        style: 'currency',
+        currency: 'EUR'
+      }
+    ).format(inputReais / euro)
+  }
+
+  if(select.value === "Bitcoin") {
+    currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+      {
+        style: 'currency',
+        currency: 'BTC'
+      }
+    ).format(inputReais / bitcoin)
+
+  }
 
 }
-    /*
-    euro
-    currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
-    { style: 'currency',
-     currency: 'EUR' }
-  ).format(inputReais / euro)
-    */
-  
 
-  //  currencyValueText.innerHTML = (inputReais / dolar)
- 
+
+
+
 const changeCurrency = () => {
-  
+
   const currencyName = document.getElementById('currency-name')
-  const currencyImg= document.getElementById('currency-img')
+  const currencyImg = document.getElementById('currency-img')
 
-  if(select.value == 'US$ Dólar Americano'){
+  if (select.value == "US$ Dólar Americano") {
 
-    currencyName.innerHTML = 'Dólar Americano'
+    currencyName.innerHTML = "Dólar Americano"
     currencyImg.setAttribute('src', 'assets/bandeira-eua.svg')
   }
 
-  if(select.value == '€ Euro'){
+  if (select.value == '€ Euro') {
     currencyName.innerHTML = '€ Euro'
     currencyImg.setAttribute('src', 'assets/euro.svg')
   }
-    
-    
+
+  if(select.value == "Bitcoin"){
+    currencyName.innerHTML = 'BitCoin'
+    currencyImg.setAttribute('src', 'assets/bitcoin.png')
   }
+
+  convertValues()
+}
 
 button.addEventListener('click', convertValues)
 select.addEventListener('change', changeCurrency)
