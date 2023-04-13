5ad79d97a14837070d36970d5091dc5c94f36eeb
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-04-13 16:14:34 -0300

    aprendendo sobre API REST

---
+++ javascript avançado/api.js
@@ -0,0 +1,22 @@
+// API ( application Programming interface)
+        // O conceito de API nada mais é do que uma forma de comunicação entre sistemas. Ou seja, elas permitem a integração entre dois sistemas, em que um deles fornece informações e serviços que podem ser utilizados pelo outro, sem a necessidade de algum dos sistemas conhecer detalhes de implementação do software.
+
+ // API REST E RESTFUL
+
+ /*
+  Rest significa representational State transfer, em portugues tranferencia de estado representacional.
+  trata-se de uma abstração da arquitetura da web. Resumidamente, o Rest consiste em principios e regras que, quando seguidos, permitem a criação de um projeto com interfaces bem definidas, dessa forma permite por exemplo, que as aplicaçoes se comuniquem.
+
+  existe uma certa confusão quanto aos termos rest e restful. Entretanto ambos representam os mesmo principios. A diferença é apenas gramatical. Em outras palavras, sistemas que utilizam os principios Rest são chamados de Restful. ser Restful é cumprir os padroes Rest
+
+  REST: Conjunto de princiopios e arquitetura
+  RESTful: uma API que segue os padroes REST
+
+
+  alguns principios REST
+
+  - cliente-servidor: as aplicaçoes existentes no cliente e no servidor devem ser separadas
+  - sem estado: as requisiçoes são feitas de forma independente, ou seja, cada uma executa apenas uma determinada tarefa.
+  - As respostas são auto-descritivas, incluindo informaçoes suficientes para que o cliente saiba como utiliza-las
+ 
+ */
