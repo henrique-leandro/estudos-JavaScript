1ab5449b8f2de7de3ea4348c9d2afb4e893987ba
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-07-25 11:10:03 -0300

    aprendendo sobre try-catch

---
+++ javascript avançado/try-catch.js
@@ -0,0 +1,13 @@
+try { 
+
+    //meu codigo aqui
+
+}catch(err){
+   // caso de algum erro posso retornar algo para o cliente
+
+   // return response.stutus(500).json({Erro: err.menssage});
+}
+
+// finally é opcinal 
+
+// como criar um novo erro no codigo : throw new Error("mensagem aqui")
